a384fe373478fec8db4359e970c6169abe2890ec
Ondřej Fibich <ondrej.fibich@gmail.com>
2014-09-04 18:08:23 +0200

    Initial commit - all sources, test, etc.

---
+++ src/PhpaxRs.php
@@ -0,0 +1,474 @@
+<?php
+
+/*
+ * This file is a part of PHPAX-RS framework, released under terms of GPL-3.0
+ * licence. Copyright (c) 2014, UnArt, o.s. All rights reserved.
+ */
+
+namespace phpaxrs;
+
+use \InvalidArgumentException;
+use \phpaxrs\common\Annotations;
+use \phpaxrs\common\DocCommentWrapper;
+use \phpaxrs\common\PathUtil;
+
+/**
+ * The "PhpaxRs" class encases functionality of the PHPAX-RS framework.
+ * It allows to specify end point classes and map them to URL paths,
+ * register serializers for implementing of many types of transport formats,
+ * and serve and render requests using matched end points.
+ * 
+ * Written for PHP 5.3 and higher.
+ *
+ * @author Ondřej Fibich <ondrej.fibich@gmail.com>
+ * 
+ * @todo support content and accept type with * (e.g. text/*)
+ * @todo handle PHP error (currently only exceptions are handled)
+ */
+class PhpaxRs {
+    
+    /**
+     * Serializators for (un)marchalling of object from request and responses.
+     * 
+     * Stored in associative array where key is accepted mine and value is
+     * name of serializator class.
+     *
+     * @var Array
+     */
+    private $serializators = array();
+    
+    /**
+     * End points for serving requests on specified path.
+     * 
+     * Stored in associative array where key is base path that is managed
+     * by endpoint and value is name of end point class.
+     *
+     * @var Array 
+     */
+    private $end_points = array();
+    
+    /**
+     * Base path to REST api. E.g. /rest-api
+     *
+     * @var string
+     */
+    private $base_path;
+
+    /**
+     * Creates PHPAX-RS API on given base path.
+     * 
+     * @param string $base_path base path from document root
+     */
+    public function __construct($base_path = '/') {
+        $this->base_path = PathUtil::normalize($base_path);
+    }
+    
+    /**
+     * Adds end point for serving requests on given base path.
+     * 
+     * @param string $base_ep_path end point base path
+     * @param string $end_point_cn class name
+     * @throws InvalidArgumentException on invalid or duplicate base path
+     */
+    public function add_endpoint($base_ep_path, $end_point_cn) {
+        if (!PathUtil::is_valid($base_ep_path)) {
+            throw new InvalidArgumentException('invalid end point path');
+        }
+        $base_ep_path = PathUtil::normalize($base_ep_path);
+        if (array_key_exists($base_ep_path, $this->end_points)) {
+            $m = 'path ' . $base_ep_path . ' already registered';
+            throw new InvalidArgumentException($m);
+        }
+        $this->end_points[$base_ep_path] = $end_point_cn;
+    }
+    
+    /**
+     * Adds serializator for (un)marchalling of object with given mine type.
+     * 
+     * @param string $mine MINE (e.g. application/json)
+     * @param string $serializator_cn class name
+     * @throws InvalidArgumentException on invalid arguments or existing mine
+     */
+    public function add_serializator($mine, $serializator_cn) {
+        if (empty($serializator_cn)) {
+            throw new InvalidArgumentException('invalid serializator');
+        }
+        $mine = mb_strtolower(trim($mine));
+        if (empty($mine)) {
+            throw new InvalidArgumentException('invalid mine: ' . $mine);
+        }
+        if (array_key_exists($mine, $this->serializators)) {
+            $m = 'serializator for mine ' . $mine . ' already exist';
+            throw new InvalidArgumentException($m);
+        }
+        $this->serializators[$mine] = $serializator_cn;
+    }
+
+    /**
+     * Creates serializator for the given mine type.
+     * 
+     * @param string $mine mine of serializator
+     * @return null|serializator\ISerializator null if not founded or instance
+     * @throws \ErrorException on invalid founded serializator (invalid sub class)
+     */
+    public function create_serializer($mine) {
+        $consumer = $this->find_serializator($mine);
+        if ($consumer === NULL) {
+            return NULL;
+        }
+        $r_consumer = new \ReflectionClass($consumer);
+        if (!$r_consumer->isSubclassOf('phpaxrs\serializator\ISerializator')) {
+            throw new \ErrorException('Invalid serializator');
+        }
+        return $r_consumer->newInstanceArgs();
+    }
+    
+    /**
+     * Serve given URL with available end points with HTTP method and headers
+     * obtained from PHP runtime.
+     * 
+     * This method can be run only if PHP is hosted by Apache server.
+     * 
+     * @param string $url
+     * @return http\HttpResponse
+     */
+    public function serve($url) {
+        return $this->serve_request(new http\HttpRequest(
+                $url, filter_input(INPUT_SERVER, 'REQUEST_METHOD'),
+                file_get_contents('php://input'),
+                getallheaders() // available only on Apache
+        ));
+    }
+    
+    /**
+     * Serve given HTTP request with available end points.
+     * 
+     * @param http\HttpRequest $request
+     * @return http\HttpResponse
+     */
+    public function serve_request($request) {
+        /* FIND END POINT FOR REQUEST */
+        
+        // get base api path
+        $full_path = $request->get_url()->get_path();
+        $path = PathUtil::relative($this->base_path, $full_path);
+        if ($path == NULL) {
+            return http\ResponseBuilder::not_found('invalid path');
+        }
+        // find end point
+        if (($ep = $this->find_end_point($path)) == NULL) {
+            return http\ResponseBuilder::not_found('no end point founded');
+        }
+        // remove matched part of the path => path for method
+        $path_method = PathUtil::relative($ep['base_path'], $path);
+        if ($path_method == NULL) {
+            return http\ResponseBuilder::not_found();
+        }
+        // init end point metaclass
+        try {
+            $r_ep = new \ReflectionClass($ep['class_name']);
+        } catch (\ReflectionException $ex) {
+            throw new \Exception('end point class not found', NULL, $ex);
+        }
+        
+        /* FIND METHODS THAT ARE CALLED WITH HTTP REQUEST METHOD */
+        
+        $methods = self::get_endpoint_methods($r_ep, $request->get_method());
+        
+        /* FILTER FOUND METHODS BY THEIR SUB PATH PATTERN AND FILL C/P INFO */
+        
+        $fmethods = self::filter_endpoint_methods_by_path($methods, $path_method);
+        
+        if (!count($fmethods)) {
+            return http\ResponseBuilder::not_found();
+        }
+        
+        $dc_ep = new DocCommentWrapper($r_ep->getDocComment());
+        
+        $cfmethods = self::fill_in_consume_info($fmethods, $request, 
+                $dc_ep->get_values(Annotations::CONSUME_MINES));
+        
+        if (!count($cfmethods)) {
+            return http\ResponseBuilder::unsupported_media();
+        }
+                        
+        $pcfmethods = self::fill_in_produce_info($cfmethods, $request, 
+                $dc_ep->get_values(Annotations::PRODUCE_MINES));
+        
+        if (!count($pcfmethods)) {
+            return http\ResponseBuilder::not_acceptable();
+        }
+        
+        /* CHOSE BEST METHOD FOR SERVING THE REQUEST */
+        
+        // we have more methods we must do some additional comparison
+        uasort($pcfmethods, function ($m1, $m2) {
+            // less count of arguments means better match
+            $cmp = count($m1['args']) - count($m2['args']);
+            if ($cmp == 0) {
+                // choose method with most priority accept content-type
+                $cmp = $m1['produces_rating'] - $m2['produces_rating'];
+            }
+            return $cmp;
+        });
+        // take first sorted item
+        $serve_method = reset($pcfmethods);
+        
+        /* SERVE REQUEST */
+        
+        $args = $serve_method['args'];
+        
+        // unmarshall input
+        if ($request->has_body() && $request->get_body() !== NULL) {
+            if (!count($serve_method['consumes'])) {
+                if (!count($request->get_content_type_header())) {
+                    // CT header missing
+                    return http\ResponseBuilder::bad_request();
+                }
+                throw new \ErrorException('@Consumes not defined');
+            }
+            $mine = reset($serve_method['consumes']);
+            $consumer = $this->create_serializer($mine);
+            if ($consumer == NULL) {
+                return http\ResponseBuilder::unsupported_media();
+            }
+            // add unmarshalled body as fist argument for serve method
+            try {
+                $obj = $consumer->unmarshall($request->get_body());
+                $args = array($obj) + $args;
+            } catch (serializator\SerializationException $ex) {
+                return http\ResponseBuilder::server_error($ex);
+            }
+            
+        }
+        
+        // call method 
+        try { // TODO: handle also PHP errors
+            $ep_instance = $r_ep->newInstanceArgs();
+            $output = $serve_method['rm']->invokeArgs($ep_instance, $args);
+        } catch (Exception $ex) {
+            return http\ResponseBuilder::server_error($ex);
+        }
+        
+        // data returned right from method? Wpar them!
+        if (!($output instanceof http\HttpResponse)) {
+            $output = http\ResponseBuilder::ok($output);
+        }
+        
+        // no output?
+        if ($output->get_body() !== NULL) {
+            // serialize body
+            $producer = NULL;
+            $content_type = NULL;
+            // find first available serializer
+            while ($producer == NULL && !empty($serve_method['produces'])) {
+                $content_type = array_shift($serve_method['produces']);
+                $producer = $this->create_serializer($content_type);
+            }
+            if ($producer == NULL) {
+                return http\ResponseBuilder::not_acceptable();
+            }
+            // marshall body and set CT header
+            $output->add_header('Content-Type', $content_type);
+            try {
+                $output->set_body($producer->marshall($output->get_body()));
+            } catch (serializator\SerializationException $ex) {
+                return http\ResponseBuilder::server_error($ex);
+            }
+        }
+        // return result
+        return $output;
+    }
+    
+    /**
+     * Renders given response to standart output.
+     * 
+     * @param \phpaxrs\http\HttpResponse $response
+     * @throws InvalidArgumentException on invalid response
+     * @throws \ErrorException if render cannot be performed
+     */
+    public function render($response) {
+        // check input
+        if (empty($response) || !($response instanceof http\HttpResponse)) {
+            throw new InvalidArgumentException('invalid response passed');
+        }
+        // check state
+        if (headers_sent()) {
+            throw new \ErrorException('headers were already sended');
+        }
+        // render status header
+        $status_code = $response->get_status();
+        $text = common\HttpUtil::status_message($status_code);
+        $protocol = filter_input(INPUT_SERVER, 'SERVER_PROTOCOL');
+        if (empty($protocol)) {
+            $protocol = 'HTTP/1.0';
+        }
+        header($protocol . ' ' . $status_code . ' ' . $text);
+        // render headers
+        foreach ($response->get_headers() as $name => $value) {
+            header($name . ': ' . $value);
+        }
+        // render body
+        echo $response->get_body();
+    }
+    
+    /**
+     * Finds end poin for the given request URL path.
+     * 
+     * @param string $request_path
+     * @return array|null array with key base_path and class_name
+     */
+    protected function find_end_point($request_path) {
+        $found = null;
+        $found_lenght = -1;
+        // / at the end of path required
+        $request_path = PathUtil::normalize($request_path);
+        // search for match in path of each end point
+        foreach ($this->end_points as $ep_bp => $ep) {
+            if (mb_strlen($ep_bp) > $found_lenght &&
+                    strncmp($request_path, $ep_bp, mb_strlen($ep_bp)) === 0) {
+                $found = $ep_bp;
+                $found_lenght = mb_strlen($found);
+            }
+        }
+        // founded?
+        if ($found) {
+            return array(
+                'base_path'  => $found,
+                'class_name' => $this->end_points[$found]
+            );
+        }
+        // not found
+        return NULL;
+    }
+    
+    /**
+     * Finds serializator that is able to serialize one of given mine types.
+     * 
+     * @param string $mine accepted mine
+     * @return string|null class name of serializator or null
+     * @throws InvalidArgumentException on invalid mines
+     */
+    protected function find_serializator($mine) {
+        if (empty($mine)) {
+            throw new InvalidArgumentException('invalid mine passed');
+        }
+        if (array_key_exists($mine, $this->serializators)) {
+            return $this->serializators[$mine];
+        }
+        return NULL;
+    }
+
+    /**
+     * Gets public implemented non-static methods of a end point that can handle
+     * given HTTP method.
+     * 
+     * @param \ReflectionClass $rep reflection end point metaclass
+     * @param string $http_method HTTP method
+     * @return Array
+     */
+    protected static function get_endpoint_methods($rep, $http_method) {
+        $http_method = strtoupper($http_method);
+        $public_methods = array_diff(
+                $rep->getMethods(\ReflectionMethod::IS_PUBLIC),
+                $rep->getMethods(\ReflectionMethod::IS_STATIC),
+                $rep->getMethods(\ReflectionMethod::IS_ABSTRACT)
+        );
+        $methods = array();
+        // filter methods that handles the same HTTP method
+        foreach ($public_methods as $method) {
+            $r_method = new \ReflectionMethod($rep->getName(), $method->name);
+            $dc_method = new DocCommentWrapper($r_method->getDocComment());
+            // check if @<method> annotation is present
+            if ($dc_method->is_present($http_method)) {
+                $methods[] = array('rm' => $r_method, 'dc' => $dc_method);
+            }
+        }
+        return $methods;
+    }
+
+    /**
+     * Filters methods with @Path annotation attribute that do not match the
+     * given path.
+     * 
+     * @param type $methods list of methods
+     * @param string $path path to be matched 
+     * @return array filtered methods with appended arguments
+     */
+    protected static function filter_endpoint_methods_by_path($methods, $path) {
+        // iterate throught each method and remove those who do not match
+        foreach ($methods as $i => $method) {
+            // get path template from @Path annotation if no @Path anntation
+            // is present than path is same as for whole end point so use "/"
+            $templ = $method['dc']->get_first_value(Annotations::URL_PATH, '/');
+            if (($args = PathUtil::match($templ, $path)) !== FALSE) {
+                $methods[$i]['args'] = $args;
+            } else {
+                unset($methods[$i]); // filter out
+            }
+        }
+        return $methods;
+    }
+    
+    /**
+     * Fills information about consuming mine types that are supported
+     * by given end point methods and filter out methods that are not
+     * capable of consuming the request.
+     * 
+     * @param array $methods list of methods for fill in
+     * @param http\HttpRequest $request request for obtaining header info
+     * @param array $cdf default consume mine type
+     * @return array filtered methods with appended consumes information
+     */
+    protected static function fill_in_consume_info(&$methods, $request, $cdf) {
+        $cdf = empty($cdf) ? array() : $cdf;
+        // iterate throught each method and fill consume/produce info field
+        foreach ($methods as $i => $method) {
+            $dc = $method['dc'];
+            $ct_mine = $request->get_content_type_header();
+            if ($ct_mine != NULL) { // CT header not present?
+                $mines = $dc->get_values(Annotations::CONSUME_MINES, $cdf);
+                $consumes = array_intersect(array($ct_mine), $mines);
+                if (!count($consumes)) {
+                    unset($methods[$i]); // filter out
+                    continue;
+                }
+                $methods[$i]['consumes'] = $consumes;
+            }
+        }
+        return $methods;
+    }
+    
+    /**
+     * Fills information about producing mine types that are supported
+     * by given end point methods and filter out methods that are not
+     * capable of produce accepted response.
+     * 
+     * @param array $methods list of methods for fill in
+     * @param http\HttpRequest $request request for obtaining header info
+     * @param array $pdf default array of produce mine types
+     * @return array filtered methods with appended produce information
+     */
+    protected static function fill_in_produce_info(&$methods, $request, $pdf) {
+        $pdf = empty($pdf) ? array() : $pdf;
+        // iterate throught each method and fill consume/produce info field
+        foreach ($methods as $i => $method) {
+            $dc = $method['dc'];
+            $accepted_mines = $request->get_accept_header();
+            $produces = $mines = $dc->get_values(Annotations::PRODUCE_MINES, $pdf);
+            $methods[$i]['produces_rating'] = PHP_INT_MAX;
+            // Accept header not present => accept everything
+            if (count($accepted_mines)) {
+                $produces = array_intersect($accepted_mines, $mines);
+                if (!count($produces)) {
+                    unset($methods[$i]); // filter out
+                    continue;
+                }
+            }
+            $methods[$i]['produces'] = $produces;
+            $methods[$i]['produces_rating'] = array_search(reset($produces), $mines);
+        }
+        return $methods;
+    }
+
+}
---
+++ src/common/HttpUtil.php
@@ -0,0 +1,118 @@
+<?php
+
+/*
+ * This file is a part of PHPAX-RS framework, released under terms of GPL-3.0
+ * licence. Copyright (c) 2014, UnArt, o.s. All rights reserved.
+ */
+
+namespace phpaxrs\common;
+
+/**
+ * The "HttpUtil" class 
+ *
+ * @author Ondřej Fibich <ondrej.fibich@gmail.com>
+ */
+class HttpUtil {
+    
+    /**
+     * List of all HTTP status messages.
+     *
+     * @var Array
+     */
+    private static $messages = array(
+		// Informational 1xx
+		100 => 'Continue',
+		101 => 'Switching Protocols',
+
+		// Success 2xx
+		200 => 'OK',
+		201 => 'Created',
+		202 => 'Accepted',
+		203 => 'Non-Authoritative Information',
+		204 => 'No Content',
+		205 => 'Reset Content',
+		206 => 'Partial Content',
+
+		// Redirection 3xx
+		300 => 'Multiple Choices',
+		301 => 'Moved Permanently',
+		302 => 'Found', // 1.1
+		303 => 'See Other',
+		304 => 'Not Modified',
+		305 => 'Use Proxy',
+		// 306 is deprecated but reserved
+		307 => 'Temporary Redirect',
+
+		// Client Error 4xx
+		400 => 'Bad Request',
+		401 => 'Unauthorized',
+		402 => 'Payment Required',
+		403 => 'Forbidden',
+		404 => 'Not Found',
+		405 => 'Method Not Allowed',
+		406 => 'Not Acceptable',
+		407 => 'Proxy Authentication Required',
+		408 => 'Request Timeout',
+		409 => 'Conflict',
+		410 => 'Gone',
+		411 => 'Length Required',
+		412 => 'Precondition Failed',
+		413 => 'Request Entity Too Large',
+		414 => 'Request-URI Too Long',
+		415 => 'Unsupported Media Type',
+		416 => 'Requested Range Not Satisfiable',
+		417 => 'Expectation Failed',
+
+		// Server Error 5xx
+		500 => 'Internal Server Error',
+		501 => 'Not Implemented',
+		502 => 'Bad Gateway',
+		503 => 'Service Unavailable',
+		504 => 'Gateway Timeout',
+		505 => 'HTTP Version Not Supported',
+		509 => 'Bandwidth Limit Exceeded'
+	);
+    
+    /**
+     * Gets message for the given HTTP status code.
+     * 
+     * @param int $status
+     * @return null|string
+     */
+    public function status_message($status) {
+        if (array_key_exists($status, self::$messages)) {
+            return self::$messages[$status];
+        }
+        return NULL;
+    }
+    
+    /**
+     * Parses HTTP accept header and returned ordered by height.
+     * 
+     * @param string $accepts HTTP accept header string
+     * @return array array of mine types
+     */
+    public static function parse_accept_header($accepts) {
+        if (empty($accepts)) {
+            return array();
+        }
+        // parse parts of accepts
+        $accepts_parts = array_map('trim', explode(',', $accepts));
+        $accepts_parsed = array();
+        // parse
+        foreach ($accepts_parts as $accepts_part) {
+            $subparts = array_map('trim', explode(';', $accepts_part));
+            $weight = 1.0;
+            if (count($subparts) > 1 && strncmp($subparts[1], 'q=', 2) == 0) {
+                $weight = floatval(substr($subparts[1], 2));
+            }
+            $accepts_parsed[$subparts[0]] = $weight;
+        }
+        // sort by weight (we need stable algorithm!)
+        array_multisort($accepts_parsed, SORT_DESC, SORT_NUMERIC,
+                range(1, count($accepts_parsed)), SORT_ASC, SORT_NUMERIC);
+        // return sorted accepts mines
+        return array_keys($accepts_parsed);
+    }
+    
+}
